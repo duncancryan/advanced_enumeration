a9771f4ee909d8501c645eaca90e4de1e8e0a595
Duncan Ryan <duncancryan1@gmail.com>
2020-08-19 23:06:51 +0100

    first two tests for isogram passing, used something called a set, which apparently has to only have unique values by necessity, and thus when a new set is instantiated while calling an array, it will only take the unique values from that array, you can then compare size of set vs size of initial array to check for duplicates

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -1,9 +1,10 @@
 const IsogramFinder = function (word) {
-
+    this.word = word;
 }
 
 IsogramFinder.prototype.isIsogram = function () {
-
+    const wordArray = this.word.split('');
+    return new Set(wordArray).size === wordArray.length;
 }
 
 module.exports = IsogramFinder;
