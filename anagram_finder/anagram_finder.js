b6693042539cb356ac1660a892867c1b4ce5e1dc
Duncan Ryan <duncancryan1@gmail.com>
2020-08-20 00:24:09 +0100

    finally got first anagram tests working, had to use a traditional for loop as couldn't get forEach to behave

--- anagram_finder/anagram_finder.js
+++ anagram_finder/anagram_finder.js
@@ -1,9 +1,27 @@
 const AnagramFinder = function (word) {
-
+    this.word = word;
+    this.wordArray = this.word.split('');
 }
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
-
+    // create sub arrays made up of letters for each word in otherWords array
+    const otherWordsSplit = otherWords.map((word) => {
+      return word.split('');
+    })
+    // create empty array to put anagrams in
+    const anagrams = [];
+    // check wordArray against these sub arrays in turn (loop but could use forEach)
+    for (var word of otherWordsSplit) {
+      if (word.every((letter) => {
+        return this.wordArray.includes(letter)
+      })) {
+        anagrams.push(word.join(''));
+      }
+    }
+    return anagrams;
+    // if includes evaluates to true return joined sub array in an array
 }
+
+
 
 module.exports = AnagramFinder;
