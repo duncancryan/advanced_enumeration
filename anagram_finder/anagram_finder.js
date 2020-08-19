b1f18525b6a324e5dc35d5d3ea24ed75e47b7cc2
Duncan Ryan <duncancryan1@gmail.com>
2020-08-20 00:30:07 +0100

    too tired now, going to have to call it a night

--- anagram_finder/anagram_finder.js
+++ anagram_finder/anagram_finder.js
@@ -4,22 +4,21 @@ const AnagramFinder = function (word) {
 }
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
-    // create sub arrays made up of letters for each word in otherWords array
     const otherWordsSplit = otherWords.map((word) => {
       return word.split('');
     })
-    // create empty array to put anagrams in
+    
     const anagrams = [];
-    // check wordArray against these sub arrays in turn (loop but could use forEach)
+    
     for (var word of otherWordsSplit) {
       if (word.every((letter) => {
         return this.wordArray.includes(letter)
-      }) && word.length === this.wordArray.length) {
+      }) && word.length === this.wordArray.length ) {
         anagrams.push(word.join(''));
       }
     }
+    
     return anagrams;
-    // if includes evaluates to true return joined sub array in an array
 }
 
 
