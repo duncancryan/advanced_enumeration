5b8690ff2e09b98833e1ab73f4c5d3f0af9a977b
Duncan Ryan <duncancryan1@gmail.com>
2020-08-19 22:19:56 +0100

    first two pangram tests passing

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -1,9 +1,14 @@
 const PangramFinder = function (phrase) {
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+  this.phrase = phrase;
 }
 
 PangramFinder.prototype.isPangram = function () {
-
+  const phraseArray = this.phrase.split('');
+  const result = this.alphabet.every((letter) => {
+    return phraseArray.includes(letter);
+  })
+  return result;
 }
 
 module.exports = PangramFinder;
