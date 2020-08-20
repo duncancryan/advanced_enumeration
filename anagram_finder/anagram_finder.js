7f8a1b8015db9daaf3262e16b2340ab8bb5b8113
Duncan Ryan <duncancryan1@gmail.com>
2020-08-20 10:30:59 +0100

    didn't like anagram solution, coming back to it later

--- anagram_finder/anagram_finder.js
+++ anagram_finder/anagram_finder.js
@@ -1,26 +1,20 @@
 const AnagramFinder = function (word) {
     this.word = word;
-    this.wordArray = this.word.split('');
 }
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
-    const otherWordsSplit = otherWords.map((word) => {
-      return word.split('');
-    })
-    
-    const anagrams = [];
-    
-    for (var word of otherWordsSplit) {
-      if (word.every((letter) => {
-        return this.wordArray.includes(letter)
-      }) && word.length === this.wordArray.length ) {
-        anagrams.push(word.join(''));
-      }
-    }
-    
-    return anagrams;
-}
+  const anagrams = [];
 
+  for (let word in otherWords) {
+    console.log(word.split('').sort.join(''));
+    }
+  }
+
+
+  return anagrams;
+}
+// move split into for loop so you can push unsplit word without join
+// above is because we need to push the original words with upperCase intact
 
 
 module.exports = AnagramFinder;
--- anagram_finder/anagram_finder_spec.js
+++ anagram_finder/anagram_finder_spec.js
@@ -22,17 +22,17 @@ describe('AnagramFinder', function () {
     assert.deepStrictEqual(anagramFinder.findAnagrams(['good']), []);
   });
 
-  xit('should detect an anagram regardless of case', function () {
+  it('should detect an anagram regardless of case', function () {
     const anagramFinder = new AnagramFinder('DeduCTionS');
     assert.deepStrictEqual(anagramFinder.findAnagrams(['DiscOUnteD']), ['DiscOUnteD']);
   });
 
-  xit('should not detect a word as it\'s own anagram', function () {
+  it('should not detect a word as it\'s own anagram', function () {
     const anagramFinder = new AnagramFinder('javascript');
     assert.deepStrictEqual(anagramFinder.findAnagrams(['javascript']), []);
   });
 
-  xit('should not detect an empty string as an anagram', function () {
+  it('should not detect an empty string as an anagram', function () {
     const anagramFinder = new AnagramFinder('word');
     assert.deepStrictEqual(anagramFinder.findAnagrams(['']), []);
   });
