e618b0946d5f53b0088c90897dc7612146045314
Duncan Ryan <duncancryan1@gmail.com>
2020-08-22 13:36:48 +0100

    all passing, looking nicer too

--- anagram_finder/anagram_finder.js
+++ anagram_finder/anagram_finder.js
@@ -1,18 +1,17 @@
 const AnagramFinder = function (word) {
     this.word = word;
+    this.sortedWord = this.word.toLowerCase().split('').sort().join('');
 }
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
-  const anagrams = [];
-
-  for (let word in otherWords) {
-    console.log(word.split('').sort.join(''));
-    }
+  anagrams = otherWords.filter((word) => {
+    return word.toLowerCase().split('').sort().join('') === this.sortedWord && word !== this.word;
+  })
+  return anagrams;
   }
 
 
-  return anagrams;
-}
+
 // move split into for loop so you can push unsplit word without join
 // above is because we need to push the original words with upperCase intact
 
